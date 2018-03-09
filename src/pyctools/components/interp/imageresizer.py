a589443d2235cdbb144457d4de293a1f2dbb809f
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2018-03-09 16:05:55 +0000

    Add 1-D filters and documentation

--- src/pyctools/components/interp/imageresizer.py
+++ src/pyctools/components/interp/imageresizer.py
@@ -17,14 +17,106 @@
 #  along with this program.  If not, see
 #  <http://www.gnu.org/licenses/>.
 
-__all__ = ['ImageResizer2D']
+__all__ = ['ImageResizerX', 'ImageResizerY', 'ImageResizer2D']
 __docformat__ = 'restructuredtext en'
 
 from pyctools.components.interp.filtergenerator import FilterGenerator
 from pyctools.components.interp.resize import Resize
 from pyctools.core.compound import Compound
 
+def ImageResizerX(config={}, **kwds):
+    """Horizontal image resizer component.
+
+    Uses a :py:class:`~pyctools.core.compound.Compound` components to
+    encapsulate :py:class:`~.resize.Resize` and
+    :py:class:`~.filtergenerator.FilterGenerator` components.
+
+    Config:
+
+    ============  ===  ====
+    ``up``        int  Up-conversion factor.
+    ``down``      int  Down-conversion factor.
+    ``aperture``  int  Filter aperture.
+    ============  ===  ====
+
+    """
+    cfg = {'aperture': 16}
+    cfg.update(kwds)
+    cfg.update(config)
+    return Compound(
+        filgen = FilterGenerator(),
+        resize = Resize(),
+        config = cfg,
+        config_map = {
+            'filgen': (('up', 'xup'), ('down', 'xdown'),
+                       ('aperture', 'xaperture')),
+            'resize': (('up', 'xup'), ('down', 'xdown')),
+            },
+        linkages = {
+            ('self',   'input')  : ('resize', 'input'),
+            ('filgen', 'output') : ('resize', 'filter'),
+            ('resize', 'output') : ('self',   'output'),
+            }
+        )
+
+def ImageResizerY(config={}, **kwds):
+    """Vertical image resizer component.
+
+    Uses a :py:class:`~pyctools.core.compound.Compound` components to
+    encapsulate :py:class:`~.resize.Resize` and
+    :py:class:`~.filtergenerator.FilterGenerator` components.
+
+    Config:
+
+    ============  ===  ====
+    ``up``        int  Up-conversion factor.
+    ``down``      int  Down-conversion factor.
+    ``aperture``  int  Filter aperture.
+    ============  ===  ====
+
+    """
+    cfg = {'aperture': 16}
+    cfg.update(kwds)
+    cfg.update(config)
+    return Compound(
+        filgen = FilterGenerator(),
+        resize = Resize(),
+        config = cfg,
+        config_map = {
+            'filgen': (('up', 'yup'), ('down', 'ydown'),
+                       ('aperture', 'yaperture')),
+            'resize': (('up', 'yup'), ('down', 'ydown')),
+            },
+        linkages = {
+            ('self',   'input')  : ('resize', 'input'),
+            ('filgen', 'output') : ('resize', 'filter'),
+            ('resize', 'output') : ('self',   'output'),
+            }
+        )
+
 def ImageResizer2D(config={}, **kwds):
+    """2-D image resizer component.
+
+    Uses a :py:class:`~pyctools.core.compound.Compound` components to
+    encapsulate two :py:class:`~.resize.Resize` and two
+    :py:class:`~.filtergenerator.FilterGenerator` components.
+
+    Using separate Resize components to filter each dimension is a lot
+    quicker than using one Resize with a 2-D filter, particularly for
+    large apertures.
+
+    Config:
+
+    =============  ===  ====
+    ``xup``        int  Horizontal up-conversion factor.
+    ``xdown``      int  Horizontal down-conversion factor.
+    ``xaperture``  int  Horizontal filter aperture.
+    ``yup``        int  Vertical up-conversion factor.
+    ``ydown``      int  Vertical down-conversion factor.
+    ``yaperture``  int  Vertical filter aperture.
+    =============  ===  ====
+
+    """
     cfg = {'xaperture': 16, 'yaperture': 16}
     cfg.update(kwds)
     cfg.update(config)
@@ -50,4 +142,3 @@ def ImageResizer2D(config={}, **kwds):
             ('xresize', 'output') : ('self',    'output'),
             }
         )
-
